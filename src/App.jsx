47d9ef88bed08a302480100dd917dcce2a2fc7e1
Luisana Fuster <125144465+luisanafh@users.noreply.github.com>
2025-01-14 20:37:33 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import {
 } from './assets/images';
 
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
-const apiKey = import.meta.env.VITE_API_KEY;
+const apiKey = '7239f7c18cda67e8981f0208fa8c87';
 
 const codes = {
 	thunderstorm: [200, 201, 202, 210, 211, 212, 221, 230, 231, 232],
